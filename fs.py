482f98090ebf31bf5f8c7c6b588e55e18aed3d77
Jacob Miller <JMILLER@ad.hc-sc.gc.ca>
2019-09-12 15:30:04 -0400

    minor revisions still no test

--- fs.py
+++ fs.py
@@ -2,7 +2,10 @@ from app import create_app, db, celery
 from app.models import User, Country, Drug, Listing, Market, Page
 
 app = create_app()
-app.app_context().push()  # I believe this was necessary for celery to access app context (i.e. access models)?
+# TODO: confirm that this can be deleted
+#app.app_context().push()  # I believe this was necessary for celery to access app context (i.e. access models)?
+                           # Since we changed the celery model, let's remove this and see if it still works
+
 
 @app.shell_context_processor
 def make_shell_context():
